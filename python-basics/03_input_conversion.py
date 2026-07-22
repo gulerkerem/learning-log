788a414e1b19b518174d330b3d3bf7a438fb8d6a
Kerem Güler <whadaks@gmail.com>
2026-07-22 19:30:21 +0300

    Python basics: input and type conversion

---
+++ python-basics/03_input_conversion.py
@@ -0,0 +1,6 @@
+city = input("Where do you live?")
+birth_year_input = input("When were you born?")
+birth_year = int(birth_year_input)
+approx_age = 2026 - birth_year
+
+print(f"You live in {city}, and you are approximately {approx_age} years old.")
