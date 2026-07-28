63a629ea532a0d860ff2c0b11efe79094fd304fc
Kerem Güler <whadaks@gmail.com>
2026-07-28 18:36:09 +0300

    Python basics: for and while loops

---
+++ python-basics/05_loops.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    if i %2 == 0:
+        print(f"{i} is Even")
+    else:
+        print(f"{i} is Odd")
+count = 0 
+while count < 5:
+    print(count)
+    count = count + 1 
