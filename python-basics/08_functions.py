5ce2b04ded5399fbb586c1239b034b2e20d57728
Kerem Güler <whadaks@gmail.com>
2026-08-04 17:52:28 +0300

    Python basics: functions

---
+++ python-basics/08_functions.py
@@ -0,0 +1,13 @@
+def is_even(number):
+    return number % 2 == 0
+message1 = is_even(4)
+message2 = is_even(7)
+message3 = is_even(11)
+print(message1)
+print(message2)
+print(message3)
+
+def calculate_age(birth_year):
+    return 2026 - birth_year
+age_result = calculate_age(2000)
+print(age_result)
