59925805bdf364be1301ed0d9383b71cc0d95e49
Kerem Güler <whadaks@gmail.com>
2026-08-07 14:23:03 +0300

    Pandas: column selection and row filtering

---
+++ pandas-practice/pandas-practice/02_selecting_filtering.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+data = {
+    "item" : ["Computer", "Car", "Phone"],
+    "price" :[3000, 25000, 1000],
+    "stock" : ["yes", "no", "yes"],
+    "Tax Ratio" : [5, 12, 6],
+    }
+
+df = pd.DataFrame(data)
+print(df["item"])
+print(df[["stock" , "item"]]) 
+print(df[df["price"] > 2000])
+print(df[df["stock"] == "yes"])
+
+      
